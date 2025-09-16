825d7a774e0ebd0962ca1f28cd8f7808a1169341
nisulnimneth <it24100010@my.sliit.lk>
2025-09-16 20:30:31 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100010_PS_Lab07.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\Nisul Nimneth\\OneDrive\\Desktop\\IT24100010")
+
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3
+1 - pnorm(130, mean=100, sd=15)
+
+qnorm(0.95, mean=100, sd=15)
+
